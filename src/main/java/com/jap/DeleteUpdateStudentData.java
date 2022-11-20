a0636df4b486dd8a798b58328ffe9bb94b8be947
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-20 23:18:34 +0530

    commit 1

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -9,21 +9,53 @@ import java.sql.*;
 public class DeleteUpdateStudentData
 {
 
-    public Connection getConnection(){
+    public  Connection getConnection(){
         Connection connection=null;
-
+        String url="jdbc:mysql://localhost:3306/school";
+        String user="Sangti";
+        String password="Atykonyak1#";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection=DriverManager.getConnection(url,user,password);
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            e.printStackTrace();
+        }
         return connection;
 
     }
 
     public void deleteStudentData(){
       // call getConnection() method
-       
         //execute delete query
+       Connection connection=getConnection();
+       String query="DELETE FROM STUDENT WHERE ROLLNO=11";
+       try {
+           Statement statement = connection.createStatement();
+          int row= statement.executeUpdate(query);
+           System.out.println(row);
+       }
+       catch (SQLException e)
+       {
+           e.printStackTrace();
+       }
+
     }
 
     public void updateStudentData(){
         // call getConnection() method
+       Connection connection=getConnection();
+       String query="UPDATE STUDENT SET STUDENT_NAME='JAITOK',AGE=20 WHERE MARKS=678 AND ROLLNO=15";
+        try {
+            Statement statement = connection.createStatement();
+            int row= statement.executeUpdate(query);
+            System.out.println(row);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
 
         //execute update query
     }
